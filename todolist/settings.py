f8326bc9513d2c4f10f08c2467a94557133a1037
Shinjan <shinjanpatra@gmail.com>
2021-12-21 23:03:44 +0530

    dfz

--- todolist/settings.py
+++ todolist/settings.py
@@ -126,7 +126,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
