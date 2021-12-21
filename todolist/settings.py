efc0ec21f46865d2d6da6b89eea235470625a51e
Shinjan <shinjanpatra@gmail.com>
2021-12-21 21:12:17 +0530

    last commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -28,9 +28,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['trakintodos.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['trakintodos.herokuapp.com']
 
 
 # Application definition
